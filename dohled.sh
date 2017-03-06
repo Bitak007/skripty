38134d565f7d053913482c6332bed6996a35f1cc
franta <bitak@gmail.com>
2017-03-06 07:49:31 +0100

    verzia pondeli

--- dohled.sh
+++ dohled.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
-
 cas=`date +%T`
-echo "Aktuální čas: "$cas
-
 ip=`ifconfig | grep Všesměr | awk -F ":" '{print $2}' | awk -F " " '{print $1}'`
-echo "Moje IP: "$ip
-
 rx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $2}' | awk -F ")" '{print $1}'`
-echo "Přijatá data: "$rx
-
 tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}' | awk -F ")" '{print $1}'`
-echo "Odeslaná data: "$tx
-
-echo "<html>" > godovsky.html
-echo "<body>">> godovsky.html
-echo $cas>> godovsky.html
-echo "<br>">> godovsky.html
-echo $ip>> godovsky.html
-echo "<br>">> godovsky.html
-echo $rx>> godovsky.html
-echo "<br>">> godovsky.html
-echo $tx>> godovsky.html
-echo "<br>">> godovsky.html
-echo "</body>">>godovsky.html
-echo "</html>">>godovsky.html
-
-
-
-
-
+lidi=`who| wc -l`
+disk=`df -h | grep sda1 | awk -F " " '{print $2}'`
+diskq=`df -h | grep sda1 | awk -F " " '{print $3}'` 
+diskw=`df -h | grep sda1 | awk -F " " '{print $4}'` 
+diske=`df -h | grep sda1 | awk -F " " '{print $5}'` 
+echo "Aktualni cas: "$cas >> /home/student/Plocha/skripty/index.html
+echo "Moje IP: "$ip >> /home/student/Plocha/skripty/index.html
+echo "Prijata data: "$rx >> /home/student/Plocha/skripty/index.html
+echo "Odeslana data: "$tx >> /home/student/Plocha/skripty/index.html
+echo "Lidi na pocitaci:"$lidi >> /home/student/Plocha/skripty/index.html
+echo "Misto na disku:"$disk >> /home/student/Plocha/skripty/index.html
+echo "Vyuzite misto na disku:"$diskq >> /home/student/Plocha/skripty/index.html
+echo "Volne misto na disku:"$diskw >> /home/student/Plocha/skripty/index.html
+echo "Vyuziti mista v % na disku:"$diske >> /home/student/Plocha/skripty/index.html
+echo "<br><br>" >> /home/student/Plocha/skripty/index.html
 
